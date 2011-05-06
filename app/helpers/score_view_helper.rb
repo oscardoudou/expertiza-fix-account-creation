84bf35c875fd0f6f2a3d8aeeeceb76aa98dbec86
Ed Gehringer <efg@ncsu.edu>
2011-05-06 16:27:06 -0400

    Changed hover-over message to be clearer

--- app/helpers/score_view_helper.rb
+++ app/helpers/score_view_helper.rb
@@ -1,4 +1,4 @@
 module ScoreViewHelper
-  CURRENT_REVIEW_CELL_MARKUP = 'style="background-color:#E0F7D7" title="Current"'
-  OUTDATED_REVIEW_CELL_MARKUP = 'style="background-color:#FAD9DD" title="Outdated"'
+  CURRENT_REVIEW_CELL_MARKUP = 'style="background-color:#E0F7D7" title="Review is current"'
+  OUTDATED_REVIEW_CELL_MARKUP = 'style="background-color:#FAD9DD" title="Review is outdated"'
 end

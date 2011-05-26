c6f18475fe724c1f6ffcf353f7b6ad8b8530e412
Ed Gehringer <efg@ncsu.edu>
2011-05-25 23:01:05 -0400

    Fixed bug in calculation of next set of deadlines. It was calculating the difference between two consecutive sets of deadlines by adding the number of days between the first deadline and last deadline in the previous round. It should not have been adding that distance.

--- app/controllers/sign_up_sheet_controller.rb
+++ app/controllers/sign_up_sheet_controller.rb
@@ -496,16 +496,16 @@ class SignUpSheetController < ApplicationController
 
         topic_deadline_subm = TopicDeadline.find_by_topic_id_and_deadline_type_id_and_round(due_date['t_id'].to_i, topic_deadline_type_subm,i)
         topic_deadline_subm.update_attributes({'due_at' => due_date['submission_' + i.to_s]})
-        flash[:error] = "Please enter a valid " + (i > 1 ? "Resubmission deadline " + (i-1).to_s : "Submission deadline") if topic_deadline_subm.errors.length > 0
+        flash[:error] = "Please enter a valid " + (i > 1 ? "resubmission deadline " + (i-1).to_s : "submission deadline") if topic_deadline_subm.errors.length > 0
         
         topic_deadline_rev = TopicDeadline.find_by_topic_id_and_deadline_type_id_and_round(due_date['t_id'].to_i, topic_deadline_type_rev,i)
         topic_deadline_rev.update_attributes({'due_at' => due_date['review_' + i.to_s]})
-        flash[:error] = "Please enter a valid Review deadline " + (i > 1 ? (i-1).to_s : "") if topic_deadline_rev.errors.length > 0
+        flash[:error] = "Please enter a valid review deadline " + (i > 1 ? (i-1).to_s : "") if topic_deadline_rev.errors.length > 0
       end
 
       topic_deadline_subm = TopicDeadline.find_by_topic_id_and_deadline_type_id(due_date['t_id'], DeadlineType.find_by_name('metareview').id)
       topic_deadline_subm.update_attributes({'due_at' => due_date['submission_' + (review_rounds+1).to_s]})
-      flash[:error] = "Please enter a valid Meta review deadline" if topic_deadline_subm.errors.length > 0
+      flash[:error] = "Please enter a valid metareview deadline" if topic_deadline_subm.errors.length > 0
     }
 
     redirect_to_sign_up(params[:assignment_id])    
@@ -591,8 +591,8 @@ class SignUpSheetController < ApplicationController
 
         set_of_due_dates.sort! {|a,b| a.due_at <=> b.due_at}
 
-        offset = (DateTime.parse(set_of_due_dates.last['due_at'].to_s) - DateTime.parse(set_of_due_dates.first['due_at'].to_s)).abs + days_between_submissions + 1
-
+#        offset = (DateTime.parse(set_of_due_dates.last['due_at'].to_s) - DateTime.parse(set_of_due_dates.first['due_at'].to_s)).abs + days_between_submissions + 1
+         offset = days_between_submissions
       end
 
       set_of_topic.each { |topic_id|

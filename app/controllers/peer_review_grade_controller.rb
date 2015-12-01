9ee06a80b245ad7b9c5e0cfc067e721c5ac61a77
yangsong8 <ysong8@ncsu.edu>
2015-11-30 23:09:01 -0500

    add comparision file and controller method

--- app/controllers/peer_review_grade_controller.rb
+++ app/controllers/peer_review_grade_controller.rb
@@ -5,7 +5,8 @@ class PeerReviewGradeController < ApplicationController
 
   def hello
     @message = "hello, here is hello function in PeerReviewGradeController"
-    teams = AssignmentTeam.where(:parent_id => 724)
+    #teams = AssignmentTeam.where(:parent_id => 724)
+    teams = AssignmentTeam.where(:parent_id => 733)
     teams.each do |team|
       quiz_questionnaire = QuizQuestionnaire.where(:instructor_id => team.id).first rescue nil
 
@@ -29,5 +30,64 @@ class PeerReviewGradeController < ApplicationController
       end
 
     end
+  end
+
+  def comparison
+    teams_b = AssignmentTeam.where(:parent_id => 733)
+    teams_a = AssignmentTeam.where(:parent_id => 724)
+    teams = teams_a + teams_b
+    @report = Hash.new
+    teams.each do |team|
+      peer_review_scores = []
+      sum_repu_hamer=0
+      sum_repu_lauw=0
+      sum_weighted_score_hamer=0
+      sum_weighted_score_lauw=0
+      sum_quiz_score=0
+      sum_weighted_score_quiz=0
+
+      peer_review_grades = PeerReviewGrade.where(:submission_id => team.id, :round => 2) # only do this for round 2
+      if !peer_review_grades.empty?
+
+        peer_review_grades.each do |peer_review_grade_record|
+          peer_review_scores << peer_review_grade_record.total_score
+
+          sum_repu_hamer += peer_review_grade_record.repu_hamer
+          sum_repu_lauw += peer_review_grade_record.repu_lauw
+
+          if peer_review_grade_record.quiz_score && peer_review_grade_record.quiz_score>=80  # quiz got 80+
+            sum_quiz_score+= peer_review_grade_record.quiz_score
+            sum_weighted_score_quiz+= peer_review_grade_record.quiz_score*peer_review_grade_record.total_score
+          elsif peer_review_grade_record.quiz_score.nil?  # no quiz score
+            sum_quiz_score+= 20.0
+            sum_weighted_score_quiz+= 20.0*peer_review_grade_record.total_score
+          else
+            sum_quiz_score+= peer_review_grade_record.quiz_score  # quiz got 80-
+            sum_weighted_score_quiz+= peer_review_grade_record.quiz_score*peer_review_grade_record.total_score
+          end
+
+          sum_weighted_score_hamer = peer_review_grade_record.repu_hamer*peer_review_grade_record.total_score
+          sum_weighted_score_lauw = peer_review_grade_record.repu_lauw*peer_review_grade_record.total_score
+
+        end
+
+        median_peer_review_score = median(peer_review_scores)
+        aggregated_by_hamer = sum_repu_hamer!=0? (sum_weighted_score_hamer/sum_repu_hamer) : "N/A"
+        aggregated_by_lauw = sum_repu_lauw!=0? (sum_weighted_score_lauw/sum_repu_lauw) : "N/A"
+        aggregated_by_quiz = sum_quiz_score!=0? (sum_weighted_score_quiz/sum_quiz_score) : "N/A"
+        num_review_done = peer_review_grades.size
+        num_quiz_done = peer_review_grades.reject {|a| a.quiz_score.nil?}.size
+
+        current_team_report ={"team_id"=>team.id, "median"=>median_peer_review_score, "agg_hamer"=>aggregated_by_hamer, "agg_lauw"=>aggregated_by_lauw,
+                              "agg_quiz"=>aggregated_by_quiz, "num_review"=>num_review_done,"num_quiz"=>num_quiz_done}
+        @report[team.id]= current_team_report
+      end
+    end
+  end
+
+  def median(array)
+    sorted = array.sort
+    len = sorted.length
+    return (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
   end
 end
